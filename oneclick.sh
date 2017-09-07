fd25f5e605832088457a11e9e7dea4716e90bcb5
amattey <eXtra#200>
2017-09-07 16:04:11 -0500

    Ready for deployment

--- oneclick.sh
+++ oneclick.sh
@@ -1,7 +1,20 @@
-mvn install
-docker stop hello-docker-app
-docker build -t hello-docker .
-#mkdir ~/docker
-docker run --name hello-docker-app -d -p 8080:8080 -v `pwd`/logs:/usr/local/tomcat/logs hello-docker
-sleep 2
-tail -f ./logs/catalina.out
+start=`date +%s`
+#!/bin/bash
+SERVICE_NAME=hello-docker-service
+TAG=latest
+DOCKER_COMPOSE_FILE_NAME=docker-compose.yml
+
+docker-compose -f ${DOCKER_COMPOSE_FILE_NAME} stop
+
+if [[ $1 != "redeploy" ]]
+then
+    mvn clean install -DskipTests=true
+    docker-compose -f ${DOCKER_COMPOSE_FILE_NAME} build
+fi
+docker-compose -f ${DOCKER_COMPOSE_FILE_NAME} up -d --force-recreate
+
+rm -rf ./logs/*
+sleep 10
+
+/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8080"
+tail -1000f ./logs/${SERVICE_NAME}.log
