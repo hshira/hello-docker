f45d0989734f344653c32bdf8220e66a7a263672
amattey <eXtra#200>
2017-09-07 15:27:13 -0500

    Upgrade Jersey

--- src/main/java/org/aws/test/helloworld/HttpTestResource.java
+++ src/main/java/org/aws/test/helloworld/HttpTestResource.java
@@ -18,6 +18,7 @@ import javax.ws.rs.core.Response;
 import java.util.*;
 
 @Component
+@Path(value = "/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class HttpTestResource {
